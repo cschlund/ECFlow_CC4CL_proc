4eb79843378b7d7ec2e1182327056d2a453d5ac2
Oliver Sus <oliver.sus@dwd.de>
2015-06-30 14:22:22 +0000

    python database call now accounts for cases when end timestamp of orbit x = start timestamp of orbit y

--- sql/list_daily_orbits.py
+++ sql/list_daily_orbits.py
@@ -6,22 +6,20 @@ import sys
 import argparse
 import datetime
 import shutil
+import fnmatch
 
 from pycmsaf.avhrr_gac.database import AvhrrGacDatabase
 from pycmsaf.argparser import datetime2pygac
 
-
-def whoischtmeischter():
-    print "VfB"
-
-def find_file(dir_contents, dtime_str, ftype):
-    for fname in dir_contents:
-        basename = os.path.basename(fname)
-        if dtime_str in basename and ftype in basename:
-            return fname
-    else:
-        raise ValueError('No entry matching datetime string {dtime_str}'
-                         .format(dtime_str=dtime_str))
+def get_file_list_via_pattern(path, pattern):
+    """
+    This function collects all files in a given
+    path which matches the given pattern.
+    """
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                return os.path.join(root, name)
 
 if __name__ == '__main__':
     if len(sys.argv) != 6:
@@ -71,20 +69,27 @@ if __name__ == '__main__':
         # Compose datetime string as created by pygac
         tstart = datetime2pygac(rec['start_time_l1c'])
 
+        path = l1_dir_current_day
+        pattern = "ECC_GAC_" + ftype + "*_" + tstart + "_*"
+
         # Search for corresponding l1c file
         if rec['start_time_l1c'].date() == previous_date and not current_date.day == 1:
             l1_files_previous_day = [pjoin(l1_dir_previous_day, fname)
                                      for fname in os.listdir(l1_dir_previous_day)]
-            fname_src = find_file(dir_contents=l1_files_previous_day, dtime_str=tstart, ftype=ftype)
+            fname_src = get_file_list_via_pattern(path, pattern)
             fname_dst = pjoin(l1_dir_current_day, os.path.basename(fname_src))
 
             # Copy file to the current day's input directory
-            shutil.copy(src=fname_src, dst=fname_dst)
+            checkFile = os.path.isfile(fname_dst)
+            if not checkFile:
+                shutil.copy(src=fname_src, dst=fname_dst)
         if rec['start_time_l1c'].date() == current_date:
-            fname_dst = find_file(dir_contents=l1_files_current_day, dtime_str=tstart, ftype=ftype)
+            fname_dst = get_file_list_via_pattern(path, pattern)
 
         if fname_dst:
             if ftype == "avhrr":
                 print fname_dst, rec['start_scanline_endcut'], rec['end_scanline_endcut'], rec['across_track']
             else:
                 print fname_dst
+
+        print
