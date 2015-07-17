bc68fe6a007da9b9116e73fb642def93311c8f1a
Oliver Sus <oliver.sus@dwd.de>
2015-07-17 07:56:46 +0000

    added new source files for parallelized extraction of daily satellite data tarfiles

---
+++ ecflow_suite/include/pbs_parallel_extract_sat_data.h
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#PBS -N %TASK%
+#PBS -q np
+#PBS -l EC_ecfs=0
+#PBS -l EC_mars=0
+#PBS -M dec5@ecmwf.int
+#PBS -l walltime=02:00:00
+#PBS -l EC_total_tasks=%NDAYS_OF_MONTH%
+#PBS -l EC_tasks_per_node=16
+#PBS -l EC_memory_per_task=3000mb
+#PBS -l EC_threads_per_task=1
+#PBS -l EC_hyperthreads=1
+
