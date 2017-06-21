d6f566edea56e67e00953ae62fda368b70348750
Oliver Sus <oliver.sus@dwd.de>
2017-06-21 06:27:34 +0000

    L3U files are now split using CDO

--- esa_routines/proc_4_archive_data.py
+++ esa_routines/proc_4_archive_data.py
@@ -8,6 +8,9 @@
 import os
 import sys
 import argparse
+import subprocess as sub
+import fnmatch
+from timeit import default_timer as timer
 
 from housekeeping import get_id, delete_dir
 from housekeeping import split_platform_string
@@ -22,6 +25,13 @@ logger = setup_root_logger(name='sissi')
 # temporary storage of L3 results
 tmp_data_storage = "/scratch/ms/de/sf7/temp_cloudcci_level3_data"
 
+def split_l3u(cdo_args, source, replace_string):
+    target = source.replace("CLD_PRODUCTS", replace_string)
+    target_interim = target + ".interim"
+    call1 = 'cdo ' + cdo_args + " " + source + " " + target_interim
+    call2 = 'nccopy -d5 ' + target_interim + " " + target
+    call3 = 'rm -f ' + target_interim
+    return " && ".join([call1, call2, call3])
 
 def l2(args_l2):
     """
@@ -95,7 +105,7 @@ def l3(args_l3):
     # -- sensor and platform settings for L3U and L3C
     if args_l3.prodtype.upper() != "L3S" and args_l3.satellite:
 
-        if args_l3.instrument.upper() == "AVHRR":
+        if args_l3.instrument.upper() == "AVHRR" and not args_l3.satellite.upper().startswith("METOP"):
             (satstr, satnum) = split_platform_string(args_l3.satellite)
             sensor = args_l3.instrument.upper()
             platform = satstr.upper() + '-' + satnum
@@ -114,6 +124,7 @@ def l3(args_l3):
                     format(args_l3.prodtype))
         sys.exit(0)
 
+        
     # -- set prodtype_name
     if args_l3.prodtype.upper() == "L3U": 
         prodtype_name = "daily_samples"
@@ -125,12 +136,13 @@ def l3(args_l3):
         logger.info("Wrong --prodtype input!")
         sys.exit(0)
 
+        
     # -- list everything in input directory
     alldirs = os.listdir(args_l3.inpdir)
 
     # -- archive now: L3U or L3C (satellite AND instrument)
     if args_l3.prodtype.upper() != "L3S":
-        
+
         # -- find latest job via ID-US number
         if len(alldirs) > 0:
             getdirs = list()
@@ -155,14 +167,84 @@ def l3(args_l3):
                         getdirs.append(ad)                    
 
             # sort list
-            getdirs.sort()
-
-            # get last element from list, should be last job
-            lastdir = getdirs.pop()
+            getdirs.sort(key=lambda s: os.path.getmtime(os.path.join(args_l3.inpdir, s)), reverse=True)
+            
+            # get first element from list, should be last job
+            lastdir = getdirs[0] 
 
             # get ID number from the last job
             idnumber = get_id(lastdir)
+                       
+            if args_l3.prodtype.upper() == "L3U":
 
+                if args_l3.write_l3u_splitlist:
+
+                    full_lastdir = os.path.join(args_l3.inpdir, lastdir)
+                    for f in os.listdir(full_lastdir):
+                        if ("CLD_PRODUCTS" in f and  f.endswith(".nc")):
+                            full_f = os.path.join(full_lastdir, f)
+                            p = sub.Popen(['cdo', 'sinfon', full_f],stdout=sub.PIPE,stderr=sub.PIPE)
+                            output, errors = p.communicate()
+                            # split at spaces, remove invalid entries and next-line strings
+                            output = filter(None, output.split(" "))
+                            output = [s.strip('\n') for s in output]
+
+                    cld_products =  'selname,' + ','.join([s for s in output if "ctt_" in s or "cth_" in s or "ctp_" in s or "cer_" in s or ("cot_" in s and not "cccot_" in s) or "cwp_" in s or "stemp_" in s or "cph_" in s or "qcflag_" in s or "time_" in s])
+                    cld_masktype =  'selname,' + ','.join([s for s in output if "cty_" in s or "cccot_" in s or "cmask_" in s or "cph_" in s or "time_" in s])
+                    cld_angles   =  'selname,' + ','.join([s for s in output if "illum_" in s or "satzen_" in s or "solzen_" in s or "relazi_" in s or "time_" in s])
+                    rad_products =  'selname,' + ','.join([s for s in output if "toa_" in s or "boa_" in s or "cla_" in s or "cee_" in s or "time_" in s])
+                    if args_l3.local:
+                        sat_measurements =  'selname,' + ','.join([s for s in output if "refl_" in s or "bt_" in s or "time_" in s])                   
+                    taskfile = open(args_l3.inpdir + "/" + "_".join([args_l3.instrument.upper(), args_l3.satellite.upper(), idnumber, "L3U_splitting_tasklist.txt"]), "w")
+
+                    for path in getdirs:
+                        full_path = os.path.join(args_l3.inpdir, path)
+                        cd = 'cd ' + full_path
+                        files = os.listdir(full_path)
+                        for l3u_file in files:
+                            if ("CLD_PRODUCTS" in l3u_file and l3u_file.endswith(".nc")):
+                                mask = split_l3u(cld_masktype, l3u_file, "CLD_MASKTYPE")
+                                ang = split_l3u(cld_angles, l3u_file, "CLD_ANGLES")
+                                rad = split_l3u(rad_products, l3u_file, "RAD_PRODUCTS")
+                                call = " && ".join([cd, mask, ang, rad])                                
+                                if args_l3.local:
+                                    toa = split_l3u(sat_measurements, l3u_file, "SAT_MEASUREMENTS")
+                                    call = call + " && " + toa
+                                prod = split_l3u(cld_products, l3u_file, "CLD_PRODUCTS")
+                                call = call + " && " + prod + "\n"
+                                taskfile.write(call)
+                    taskfile.close()
+                    
+                    sys.exit(0)
+
+                if args_l3.check_splitting:
+                    
+                    for root, dirs, files in os.walk(args_l3.inpdir):
+                        for name in files:
+                            if idnumber in name:
+                                taskfile_name = os.path.join(root, name)
+                                with open(taskfile_name) as f:
+                                    taskfile = f.readlines()
+                                    taskfile = [x.strip() for x in taskfile]                                                                
+                        for name in dirs:
+                            matches = []
+                            if name.endswith(idnumber):
+                                dirfiles =  os.listdir(os.path.join(root, name))
+                                for filename in fnmatch.filter(dirfiles, '*.nc'):
+                                    matches.append(filename)
+                                nfiles= len(matches)
+                                if (nfiles < 4 and not args_l3.local) or (nfiles < 5 and args_l3.local):
+                                    print "Not enough NetCDF output files in " + name + ", so will split sequentially"
+                                    yyyymmdd = name[0:8]
+                                    for line in taskfile:
+                                        if yyyymmdd in line:
+                                            cwd = line.split()[1]
+                                            calls = line.split(" && ")[1:]
+                                            for call in calls:
+                                                process = sub.Popen(call.split(), stdout=sub.PIPE, stderr=sub.PIPE, cwd=cwd)
+                                                output, error = process.communicate()                                                                
+                    sys.exit(0)
+                        
             # make tarfile
             (tlist, tempdir) = tar_results(args_l3.prodtype.upper(), args_l3.inpdir, 
                                            datestring, sensor, platform, idnumber, args_l3.local)
@@ -278,6 +360,12 @@ if __name__ == '__main__':
                            help="Choices: \'L3U\', \'L3C\', \'L3S\'")
     l3_parser.add_argument('-loc', '--local', action="store_true",
                            help="Logical, TRUE if set, otherwise FALSE")
+    l3_parser.add_argument('--write_l3u_splitlist', action="store_true",
+                           help="Logical, TRUE if set, otherwise False")
+    l3_parser.add_argument('--check_splitting', action="store_true",
+                           help="Logical, TRUE if set, otherwise False")
+    l3_parser.add_argument('--proc_toa', type=int, required=True,
+                           help="Choices: 0, 1")    
     l3_parser.set_defaults(func=l3)
 
     # Parse arguments
